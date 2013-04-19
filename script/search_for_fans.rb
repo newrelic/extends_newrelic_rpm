b6142b4bd42677ac6ac9c56626ae2039be96574f
Sam Goldstein <sam@newrelic.com>
2013-04-18 18:18:07 -0700

    Look for just added gems too

--- script/search_for_fans.rb
+++ script/search_for_fans.rb
@@ -1,19 +1,21 @@
 require 'json'
 # get the last 50 updated gems
-latest = `curl -s 'https://rubygems.org/api/v1/activity/just_updated.json'`
+%w| https://rubygems.org/api/v1/activity/latest https://rubygems.org/api/v1/activity/just_updated.json|.each do |api|
+  latest = `curl -s '#{api}'`
 
-gems = JSON(latest)
-gems.each do |gem| 
-  gem['dependencies'].each do |gem_dependencies|
-    dependencies = gem_dependencies && gem_dependencies.last.select{|d| d['name'] == 'newrelic_rpm'}
-    dependencies.each do |dependency|
-      puts "#{gem['name']} #{gem['version']} is dependent on #{dependency['name']} #{dependency['version']} (#{dependency.inspect})"
-      github_url = gem.map{|kv| (kv[0] =~ /uri/) ? (kv[1]) : nil}.compact.grep(%r|https?://github\.com/|).first
+  gems = JSON(latest)
+  gems.each do |gem| 
+    gem['dependencies'].each do |gem_dependencies|
+      dependencies = gem_dependencies && gem_dependencies.last.select{|d| d['name'] == 'newrelic_rpm'}
+      dependencies.each do |dependency|
+        puts "#{gem['name']} #{gem['version']} is dependent on #{dependency['name']} #{dependency['version']} (#{dependency.inspect})"
+        github_url = gem.map{|kv| (kv[0] =~ /uri/) ? (kv[1]) : nil}.compact.grep(%r|https?://github\.com/|).first
 
-      if github_url
-        repo_url = github_url.sub(%r|https?://github\.com/|, 'git@github.com:')
-        cmd = "bash #{File.expand_path(File.join(File.dirname(__FILE__), 'add'))}"
-        puts `#{cmd} '#{repo_url.gsub("'", "")}'`
+        if github_url
+          repo_url = github_url.sub(%r|https?://github\.com/|, 'git@github.com:')
+          cmd = "bash #{File.expand_path(File.join(File.dirname(__FILE__), 'add'))}"
+          puts `#{cmd} '#{repo_url.gsub("'", "")}'`
+        end
       end
     end
   end
